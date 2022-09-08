14b3cb0b772aa12267c338119112f80512dd617a
TabithaKoech <tabbyk1718@gmail.com>
2022-09-08 22:35:03 +0300

    added a one to many relationship to our cocktails model

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,5 +1,5 @@
 class Cocktail < ActiveRecord::Base
-    has_many :reviews
+    has_many :reviews//one to many relationship
     validates :cocktail_name, presence: true, uniqueness: true
     
 end
