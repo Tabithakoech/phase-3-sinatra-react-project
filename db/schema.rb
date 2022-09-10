78f8c1ea092e136188c7fdd76109464f7e6af5b2
TabithaKoech <tabbyk1718@gmail.com>
2022-09-10 22:02:18 +0300

    did migrations for tables to create the schema

---
+++ db/schema.rb
@@ -0,0 +1,42 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2022_09_10_185804) do
+
+  create_table "departments", force: :cascade do |t|
+    t.string "department_name"
+    t.string "address"
+    t.string "phone"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "employees", force: :cascade do |t|
+    t.string "employee_name"
+    t.integer "age"
+    t.string "gender"
+    t.string "phone"
+    t.boolean "active"
+    t.integer "department_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.integer "teacher_id"
+    t.integer "employee_id"
+    t.date "date"
+    t.index ["employee_id"], name: "index_tasks_on_employee_id"
+    t.index ["teacher_id"], name: "index_tasks_on_teacher_id"
+  end
+
+end
